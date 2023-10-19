5c4c1ec37a682c8c0b4e2d6e84cdd430164c48d2
Haise17 <sam_here@outlook.com>
2023-10-19 17:39:20 +0500

    fetches data from store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+const firebaseConfig = {
+    // doesn't work without the mock string
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp({
+    // your config
+    ...firebaseConfig,
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // user object or null
+    const [user] = useAuthState(auth)
+
+    return (
+        <div className="App">
+
+            {/* <header className="App-header"> */}
+
+            {/* </header> */}
+
+            <section>
+                {user ? <ChatRoom /> : <SignIn />}
+            </section>
+
+        </div>
+    );
+}
+
+function SignIn() {
+
+    const useSignInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        auth.signInWithPopup(provider)
+    }
+
+    return (
+        <button onClick={useSignInWithGoogle}>Sign in with Google</button>
+    )
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+function ChatRoom() {
+
+    const messagesRef = firestore.collection('messages')
+    const query = messagesRef.orderBy('createdAt').limit(25)
+
+    // fetch and listen to data
+    const [messages] = useCollectionData(query, { idField: 'id' })
+
+    return (
+        <>
+            <div>
+                {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+            </div>
+        </>
+    )
+
+}
+
+function ChatMessage(props) {
+    const { text, uid } = props.message
+
+    return (
+        <p>{text}</p>
+    )
 }
 
 export default App;
