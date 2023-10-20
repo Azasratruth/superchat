4807092fc64b1b9e9770285be4675b0f49c43abb
Haise17 <sam_here@outlook.com>
2023-10-20 11:52:42 +0500

    added the code for functions

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -17,3 +17,27 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const functions = require('firebase-functions')
+const Filter = require('bad-words')
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const db = admin.firestore()
+
+exports.detectEvilUsers = functions.firestore
+    .document('messages')
+    .onCreate(async (doc, ctx) => {
+
+        const filter = new Filter()
+        const { text, uid } = doc.data()
+
+        if (filter.isProfane(text)) {
+            const cleaned = filter.clean(text)
+            await doc.ref.update({ text: `🤐 I got BANNED for life for saying... ${cleaned}` })
+            await db.collection('banned').doc(uid).set({})
+        }
+
+    })
